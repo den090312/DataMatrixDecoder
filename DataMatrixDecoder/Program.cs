a06e9a863949d2e25c18d3d1dc26102ea1fa0bf9
Денис Болотин <den090312@gmail.com>
2021-01-29 13:25:52 +0400

    start

--- DataMatrixDecoder/Program.cs
+++ DataMatrixDecoder/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Libdmtx;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,20 @@ namespace DataMatrixDecoder
     {
         static void Main(string[] args)
         {
+            var fileName = "1185754.jpg";
+            //var fileName = "1201614.jpg";
+            //var fileName = "wiki.png";
+
+            var filePath = $@"{AppDomain.CurrentDomain.BaseDirectory}{fileName}";
+
+            DecodeOptions o = new DecodeOptions();
+            Bitmap b = new Bitmap(filePath);
+            DmtxDecoded[] res = Dmtx.Decode(b, o);
+            for (uint i = 0; i < res.Length; i++)
+            {
+                string str = Encoding.ASCII.GetString(res[i].Data).TrimEnd('\0');
+                Console.WriteLine("Code " + i + ": " + str);
+            }
         }
     }
 }
